17b1fcaac461dd93f91c88e884f9423586ad37a3
Neale Forrest <neale.forrest@gmail.com>
2019-02-10 17:58:18 +0200

    Added the link to Codewars on the Kyu

--- episode_000/generateReadme.js
+++ episode_000/generateReadme.js
@@ -1,21 +1,19 @@
-const { promises: fs } = require('fs');
-const axios = require('axios');
-const { items: videos } = require('./videos.json');
+const { promises: fs } = require("fs");
+const axios = require("axios");
+const { items: videos } = require("./videos.json");
 
 (async () => {
-  let tableOfContents = '';
-  const files = await fs.readdir('../', 'utf8');
+  let tableOfContents = "";
+  const files = await fs.readdir("../", "utf8");
   files.reverse();
   for (const directory of files) {
-    if (directory.startsWith('episode') && directory !== 'episode_000') {
-      const episodeNum = +directory.replace('episode_', '');
-      const solutions = await fs.readdir(`../${directory}`, 'utf8');
+    if (directory.startsWith("episode") && directory !== "episode_000") {
+      const episodeNum = +directory.replace("episode_", "");
+      const solutions = await fs.readdir(`../${directory}`, "utf8");
 
       const {
         snippet: {
-          resourceId: {
-            videoId
-          }
+          resourceId: { videoId }
         }
       } = videos[episodeNum - 1];
 
@@ -24,24 +22,29 @@ const { items: videos } = require('./videos.json');
 
 [Watch on YouTube](https://www.youtube.com/watch?v=${videoId})
 
-`
+`;
       for (const name of solutions) {
-        if (name.endsWith('.js')) {
-          const slug = name.replace('.js', '');
+        if (name.endsWith(".js")) {
+          const slug = name.replace(".js", "");
           const link = `[${slug}](./${directory}/${name})`;
           try {
-            const { data: { rank: { name: kyu } } } = await axios.get(`https://www.codewars.com/api/v1/code-challenges/${slug}`);
-            console.log(slug, kyu);
-            tableOfContents += `* ${link} - ${kyu}\n`
+            const {
+              data: {
+                url,
+                rank: { name: kyu }
+              }
+            } = await axios.get(
+              `https://www.codewars.com/api/v1/code-challenges/${slug}`
+            );
+            console.log(slug, kyu, url);
+            tableOfContents += `* ${link} - [${kyu}](${url})\n`;
           } catch (error) {
-            tableOfContents += `* ${link} - ??? kyu\n`
+            tableOfContents += `* ${link} - ??? kyu\n`;
           }
-
         }
       }
     }
   }
 
-  await fs.writeFile('contents.md', tableOfContents, 'utf8');
+  await fs.writeFile("contents.md", tableOfContents, "utf8");
 })();
-
